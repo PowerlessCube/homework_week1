4f46e0170107a582e3db81ef220c66324d81e081
Alistair Mackay <alistair.mackay.work@gmail.com>
2016-04-30 17:19:12 +0100

    realised I accidentally removed my warehouse. Derp

--- warehouse_house_picker/warehouse_picker_functions.rb
+++ warehouse_house_picker/warehouse_picker_functions.rb
@@ -1,5 +1,38 @@
 require( "pry-byebug" )
 
+WAREHOUSE = {
+      :a10 => "rubberband",
+      :a9 => "glow stick",
+      :a8 => "model car",
+      :a7 => "bookmark",
+      :a6 => "shovel",
+      :a5 => "rubber duck",
+      :a4 => "hanger",
+      :a3 => "blouse",
+      :a2 => "stop sign",
+      :a1 => "needle",
+      :c1 => "rusty nail",
+      :c2 => "drill press",
+      :c3 => "chalk",
+      :c4 => "word search",
+      :c5 => "thermometer",
+      :c6 => "face wash",
+      :c7 => "paint brush",
+      :c8 => "candy wrapper",
+      :c9 => "shoe lace",
+      :c10 => "leg warmers",
+      :b1 => "tire swing",
+      :b2 => "sharpie",
+      :b3 => "picture frame",
+      :b4 => "photo album",
+      :b5 => "nail filer",
+      :b6 => "toothpaste",
+      :b7 => "bath fizzers",
+      :b8 => "tissue box",
+      :b9 => "deodorant",
+      :b10 => "cookie jar"
+    }
+
     def search_by_shelf(bay)
       WAREHOUSE[bay]
     end
