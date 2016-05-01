b9ea14caae944cf2f5463016496972417d0086fe
Alistair Mackay <alistair.mackay.work@gmail.com>
2016-05-01 15:13:43 +0100

    Changed data structure via Zsolt solution of using array and was able to complete all questions

--- warehouse_house_picker/specs/ware_house_picker_spec.rb
+++ warehouse_house_picker/specs/ware_house_picker_spec.rb
@@ -6,23 +6,36 @@ class TestWarehouse < Minitest::Test
 
   def test_search_by_shelf
     item = search_by_shelf(:b5)
-    assert_equal('nail filer', item)
+    assert_equal([24, 'nail filer'], item)
   end
 
   def test_search_by_item
     shelf = search_by_item('nail filer')
-    assert_equal(:b5, shelf)
+    assert_equal([24, :b5], shelf)
   end
 
   def test_search_by_list_of_shelves
     list_of_shelves = search_by_list_of_shelves([:b5, :b10, :b6])
-    assert_equal(["nail filer", "cookie jar", "toothpaste"], list_of_shelves)
+    assert_equal(["nail filer", "cookie jar", "toothpaste", 5], list_of_shelves)
   end
 
   def test_search_by_list_of_items
-    list_of_items = search_by_list_of_items(["nail filer", "cookie jar", "toothpaste"])
-    assert_equal([:b5, :b10, :b6], list_of_items)
+    list_of_items = search_by_list_of_items(["shoe lace", "rusty nail", "leg warmers"])
+    assert_equal([:c1, :c9, :c10], list_of_items)
   end
+
+#given "b3, c7, c9 and a3", determine that the products are "picture frame, paint brush, shoe lace, and blouse", and they're 15 bays apart.)
+  def test_search_by_bigger_list_of_shelves
+    list_of_shelves = search_by_list_of_shelves([:b3, :c7, :c9, :a3])
+    assert_equal(["picture frame", "paint brush", "shoe lace", "blouse", 15], list_of_shelves)
+  end
+
+#given "hanger, deodorant, candy wrapper, rubber band", determine that those items need to be collected from "a10, a4, c8, and b9"
+  def test_search_by_bigger_list_of_items
+    list_of_items = search_by_list_of_items(["hanger", "deodorant", "candy wrapper", "rubberband"])
+    assert_equal([:a10, :a4, :c8, :b9], list_of_items)
+  end
+
 
 end
 
--- warehouse_house_picker/warehouse_picker_functions.rb
+++ warehouse_house_picker/warehouse_picker_functions.rb
@@ -1,63 +1,102 @@
 require( "pry-byebug" )
 
-WAREHOUSE = {
-      :a10 => "rubberband",
-      :a9 => "glow stick",
-      :a8 => "model car",
-      :a7 => "bookmark",
-      :a6 => "shovel",
-      :a5 => "rubber duck",
-      :a4 => "hanger",
-      :a3 => "blouse",
-      :a2 => "stop sign",
-      :a1 => "needle",
-      :c1 => "rusty nail",
-      :c2 => "drill press",
-      :c3 => "chalk",
-      :c4 => "word search",
-      :c5 => "thermometer",
-      :c6 => "face wash",
-      :c7 => "paint brush",
-      :c8 => "candy wrapper",
-      :c9 => "shoe lace",
-      :c10 => "leg warmers",
-      :b1 => "tire swing",
-      :b2 => "sharpie",
-      :b3 => "picture frame",
-      :b4 => "photo album",
-      :b5 => "nail filer",
-      :b6 => "toothpaste",
-      :b7 => "bath fizzers",
-      :b8 => "tissue box",
-      :b9 => "deodorant",
-      :b10 => "cookie jar"
-    }
+WAREHOUSE = [
+      {:a10 => "rubberband"},
+      {:a9 => "glow stick"},
+      {:a8 => "model car"},
+      {:a7 => "bookmark"},
+      {:a6 => "shovel"},
+      {:a5 => "rubber duck"},
+      {:a4 => "hanger"},
+      {:a3 => "blouse"},
+      {:a2 => "stop sign"},
+      {:a1 => "needle"},
+      {:c1 => "rusty nail"},
+      {:c2 => "drill press"},
+      {:c3 => "chalk"},
+      {:c4 => "word search"},
+      {:c5 => "thermometer"},
+      {:c6 => "face wash"},
+      {:c7 => "paint brush"},
+      {:c8 => "candy wrapper"},
+      {:c9 => "shoe lace"},
+      {:c10 => "leg warmers"},
+      {:b1 => "tire swing"},
+      {:b2 => "sharpie"},
+      {:b3 => "picture frame"},
+      {:b4 => "photo album"},
+      {:b5 => "nail filer"},
+      {:b6 => "toothpaste"},
+      {:b7 => "bath fizzers"},
+      {:b8 => "tissue box"},
+      {:b9 => "deodorant"},
+      {:b10 => "cookie jar"}
+    ]
 
-    def search_by_shelf(bay)
-      WAREHOUSE[bay]
+def search_by_shelf(bay)
+  index = 0
+  while index < WAREHOUSE.length
+    if WAREHOUSE[index][bay] != nil
+      return [index, WAREHOUSE[index][bay]]
     end
+    index += 1
+  end
+end
+#My favourite answer for this question 1
+    # def search_by_shelf(bay)
+    #   WAREHOUSE[bay]
+    # end
 
     def search_by_item(item)
-      WAREHOUSE.key(item)
+      index = 0
+      while index < WAREHOUSE.length
+        if WAREHOUSE[index].key(item) != nil
+          return [index, WAREHOUSE[index].key(item)]
+        end
+        index += 1
+      end
     end
+
+#My favourite answer for this question 2
+    # def search_by_item(item)
+    #   WAREHOUSE.key(item)
+    # end
 
     def search_by_list_of_shelves(list_of_shelves)
-    list_of_items = []
+
+      items_location = []
+      locations = []
+      items = []
+
       for shelf in list_of_shelves
-        list_of_items.push(search_by_shelf(shelf))
+        items_location.push(search_by_shelf(shelf))
       end
-      return list_of_items
-    end
+      for item in items_location
+        locations.push(item[0])
+        items.push(item[1])
+      end
 
+      return items << (locations.max - locations.min)
+
+    end
+    #
     def search_by_list_of_items(list_of_items)
-      list_of_shelves = []
-        for items in list_of_items
-          list_of_shelves.push(search_by_item(items))
+      shelf_locations = []
+      shelves = []
+
+        for item in list_of_items
+          shelf_locations.push(search_by_item(item))
         end
-        return list_of_shelves
+        for shelf in shelf_locations.sort
+#sorting array by index within shelf_locations.
+          shelves.push(shelf[1])
+        end
+        return shelves
     end
-
-
+    #
+    # def get_distance()
+    #
+    # end
 
 
 nil
